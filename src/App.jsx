5774de190c9840363f6e24cd86d45c4df43a1a9f
miranda-leticia19 <142825970+miranda-leticia19@users.noreply.github.com>
2023-09-21 01:30:40 +0000

    Emplementação

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <h2>Maya Bishop</h2>
+        <img src= "https://preview.redd.it/maya-bishop-has-been-voted-station-19s-favourite-character-v0-0rjoi6aqabhb1.jpg?width=640&crop=smart&auto=webp&s=0d3426594db90477f9f169b08fa02c53c2434b1c" />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
