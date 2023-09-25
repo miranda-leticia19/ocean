e9a1a2fda9aef2df90d257f49888f72ee51a04eb
miranda-leticia19 <142825970+miranda-leticia19@users.noreply.github.com>
2023-09-25 23:43:00 +0000

    atualização do codigo

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,22 @@ function App() {
   return (
     <>
       <div className="card">
-        <h2>Maya Bishop</h2>
+        <h2>Maya DeLuca-Bishop</h2>
+        <div className= "tags">
+          <div className= "tag">Status: Vivo</div>
+          <div className= "tag">Profissão: Bombeira</div>
+          <div className= "tag">Origem: Seattle</div>
+        </div>
         <img src= "https://preview.redd.it/maya-bishop-has-been-voted-station-19s-favourite-character-v0-0rjoi6aqabhb1.jpg?width=640&crop=smart&auto=webp&s=0d3426594db90477f9f169b08fa02c53c2434b1c" />
+      </div>
+      <div className="card">
+        <h2> Carina Deluca-Bishop</h2>
+        <div className= "tags">
+          <div className= "tag">Status: Vivo</div>
+          <div className= "tag">Profissão: OB/Ginecologista</div>
+          <div className= "tag">Origem: Sícilia</div>
+        </div>
+        <img src= "https://static.wikia.nocookie.net/greysanatomy/images/f/f8/S196x17-3.jpg/revision/latest/scale-to-width-down/1000?cb=20230504024209" />
       </div>
     </>
   )
