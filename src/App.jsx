709f2e9d99e0d626c1d7a4f9eff3f5e238c2f7c6
miranda-leticia19 <142825970+miranda-leticia19@users.noreply.github.com>
2023-09-27 18:03:15 +0000

    correção

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,28 @@ import './App.css'
 import Card from './components/Card/Card'
 
 function App() {
+  const item1 = {
+    nome: "Maya Deluca-Bishop",
+    imagemUrl: 'https://preview.redd.it/maya-bishop-has-been-voted-station-19s-favourite-character-v0-0rjoi6aqabhb1.jpg?width=640&crop=smart&auto=webp&s=0d3426594db90477f9f169b08fa02c53c2434b1c'
+  }
+
+  const item2 = {
+    nome: 'Carina Deluca-Bishop',
+    imagemUrl: 'https://static.wikia.nocookie.net/greysanatomy/images/f/f8/S196x17-3.jpg/revision/latest/scale-to-width-down/1000?cb=20230504024209'
+  }
+
+  const item3 = {
+    nome: 'Travis  Montgomery ',
+    imagemUrl: 'https://static.wikia.nocookie.net/greysanatomy/images/1/18/S196x08-11.jpg/revision/latest/scale-to-width-down/1000?cb=20230222031905'
+  }
+
+  const itens = [item1, item2, item3]
+
   return (
     <>
-      <Card />
-      <div className="card">
-        <h2>Maya DeLuca-Bishop</h2>
-        <div className= "tags">
-          <div className= "tag">Status: Vivo</div>
-          <div className= "tag">Profissão: Bombeira</div>
-          <div className= "tag">Origem: Seattle</div>
-        </div>
-        <img src= "https://preview.redd.it/maya-bishop-has-been-voted-station-19s-favourite-character-v0-0rjoi6aqabhb1.jpg?width=640&crop=smart&auto=webp&s=0d3426594db90477f9f169b08fa02c53c2434b1c" />
-      </div>
-      <div className="card">
-        <h2> Carina Deluca-Bishop</h2>
-        <div className= "tags">
-          <div className= "tag">Status: Vivo</div>
-          <div className= "tag">Profissão: OB/Ginecologista</div>
-          <div className= "tag">Origem: Sícilia</div>
-        </div>
-        <img src= "https://static.wikia.nocookie.net/greysanatomy/images/f/f8/S196x17-3.jpg/revision/latest/scale-to-width-down/1000?cb=20230504024209" />
-      </div>
+      {itens.map(function (item) {
+        return <Card item={item}/>
+      })}
     </>
   )
 }
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,5 +1,20 @@
 import "./Card.css"
 
-export default function Card(){
-    return <div>Card</div>
+export default function Card(props) {
+  const item = props.item;
+
+  return <div className="card">
+    <h2>{item.nome}</h2>
+
+    <div className="tags">
+      <div className="tag">Status: Vivo</div>
+      <div className="tag">Mora: Seattle</div>
+    </div>
+
+    <img src={item.imagemUrl} />
+  </div>
 }
+
+
+
+
